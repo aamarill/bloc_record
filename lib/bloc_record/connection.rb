ac2a30fce60bb245b0605250241be3fbf27e5ba6
Adan Amarillas <aamarill.engr@gmail.com>
2018-03-04 19:14:38 -0600

    Begin work on 'Associations' assignment

--- lib/bloc_record/connection.rb
+++ lib/bloc_record/connection.rb
@@ -2,6 +2,11 @@ require 'sqlite3'
 
 module Connection
   def connection
-    @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    if BlocRecord.platform == 'sqlite'
+      @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    elsif BlocRecord.platform == 'pq'
+      # Look up what the sytnax is for this
+      @connection ||= #POSTGRES???.new(BlocRecord.database_filename)
+    end
   end
 end
